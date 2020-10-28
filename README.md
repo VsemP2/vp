e482c9e1e456483caf5396ca0aecf81245246cf5
VsemP2 <vsemp2@hotmail.com>
2020-10-29 01:18:54 +0200

    make it better

---
+++ README.md
@@ -0,0 +1,5 @@
+# CRUD Stickers
+
+## END
+
+![](https://lucid.app/publicSegments/view/b584116d-f48f-45bc-ab3f-dc2b94f0d243/image.png)
---
+++ knexfile.js
@@ -0,0 +1,59 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    // client: 'pg',
+    // connection: 'postgres://localhost/cjs-web-store'
+    client: 'postgresql',
+    connection: {
+      database: 'cjs-web-store',
+      user:     'vsemp',
+      password: 'wsaqre13'
+    },
+  },
+
+  test: {
+    // client: 'pg',
+    // connection: 'postgres://localhost/cjs-web-store'
+    client: 'postgresql',
+    connection: {
+      database: 'test-cjs-web-store',
+      user:     'vsemp',
+      password: 'wsaqre13'
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'cjs-web-store',
+      user:     'vsemp',
+      password: 'wsaqre13'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'cjs-web-store',
+      user:     'vsemp',
+      password: 'wsaqre13'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
